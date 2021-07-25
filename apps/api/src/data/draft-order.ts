8b5a1f0670b262f379648a93293ee81f19540317
Zarar Siddiqi <zarars@gmail.com>
2021-07-24 20:58:02 -0400

    Team logos

--- apps/api/src/data/draft-order.ts
+++ apps/api/src/data/draft-order.ts
@@ -1,242 +1,302 @@
 export const draftOrder = [
   {
     "order": 1,
+    "team_id": 8,
     "team": "Detroit"
   },
   {
     "order": 2,
+    "team_id": 22,
     "team": "Houston"
   },
   {
     "order": 3,
+    "team_id": 7,
     "team": "Cleveland"
   },
   {
     "order": 4,
+    "team_id": 5,
     "team": "Toronto"
   },
   {
     "order": 5,
+    "team_id": 14,
     "team": "Orlando"
   },
   {
     "order": 6,
+    "team_id": 28,
     "team": "Oklahoma City"
   },
   {
     "order": 7,
+    "team_id": 16,
     "team": "Golden State"
   },
   {
     "order": 8,
+    "team_id": 14,
     "team": "Orlando"
   },
   {
     "order": 9,
+    "team_id": 20,
     "team": "Sacramento"
   },
   {
     "order": 10,
+    "team_id": 24,
     "team": "New Orleans"
   },
   {
     "order": 11,
+    "team_id": 12,
     "team": "Charlotte"
   },
   {
     "order": 12,
+    "team_id": 25,
     "team": "San Antonio"
   },
   {
     "order": 13,
+    "team_id": 9,
     "team": "Indiana"
   },
   {
     "order": 14,
+    "team_id": 16,
     "team": "Golden State"
   },
   {
     "order": 15,
+    "team_id": 15,
     "team": "Washington"
   },
   {
     "order": 16,
+    "team_id": 16,
     "team": "Oklahoma City"
   },
   {
     "order": 17,
+    "team_id": 23,
     "team": "Memphis"
   },
   {
     "order": 18,
+    "team_id": 18,
     "team": "Oklahoma City"
   },
   {
     "order": 19,
+    "team_id": 3,
     "team": "New York"
   },
   {
     "order": 20,
+    "team_id": 11,
     "team": "Atlanta"
   },
   {
     "order": 21,
+    "team_id": 3,
     "team": "New York"
   },
   {
     "order": 22,
+    "team_id": 18,
     "team": "Los Angeles Lakers"
   },
   {
     "order": 23,
+    "team_id": 22,
     "team": "Houston"
   },
   {
     "order": 24,
+    "team_id": 22,
     "team": "Houston"
   },
   {
     "order": 25,
+    "team_id": 17,
     "team": "LA Clippers"
   },
   {
     "order": 26,
+    "team_id": 26,
     "team": "Denver"
   },
   {
     "order": 27,
+    "team_id": 2,
     "team": "Brooklyn"
   },
   {
     "order": 28,
+    "team_id": 4,
     "team": "Philadelphia"
   },
   {
     "order": 29,
+    "team_id": 19,
     "team": "Phoenix"
   },
   {
     "order": 30,
+    "team_id": 30,
     "team": "Utah"
   },
   {
     "order": 31,
+    "team_id": 10,
     "team": "Milwaukee"
   },
   {
     "order": 32,
+    "team_id": 3,
     "team": "New York"
   },
   {
     "order": 33,
+    "team_id": 14,
     "team": "Orlando"
   },
   {
     "order": 34,
+    "team_id": 28,
     "team": "Oklahoma City"
   },
   {
     "order": 35,
+    "team_id": 24,
     "team": "New Orleans"
   },
   {
     "order": 36,
+    "team_id": 28,
     "team": "Oklahoma City"
   },
   {
     "order": 37,
+    "team_id": 8,
     "team": "Detroit"
   },
   {
     "order": 38,
+    "team_id": 6,
     "team": "Chicago"
   },
   {
     "order": 39,
+    "team_id": 20,
     "team": "Sacramento"
   },
   {
     "order": 40,
+    "team_id": 24,
     "team": "New Orleans"
   },
   {
     "order": 41,
+    "team_id": 25,
     "team": "San Antonio"
   },
   {
     "order": 42,
+    "team_id": 8,
     "team": "Detroit"
   },
   {
     "order": 43,
+    "team_id": 24,
     "team": "New Orleans"
   },
   {
     "order": 44,
+    "team_id": 2,
     "team": "Brooklyn"
   },
   {
     "order": 45,
+    "team_id": 1,
     "team": "Boston"
   },
   {
     "order": 46,
+    "team_id": 5,
     "team": "Toronto"
   },
   {
     "order": 47,
+    "team_id": 5,
     "team": "Toronto"
   },
   {
     "order": 48,
+    "team_id": 11,
     "team": "Atlanta"
   },
   {
     "order": 49,
+    "team_id": 2,
     "team": "Brooklyn"
   },
   {
     "order": 50,
+    "team_id": 4,
     "team": "Philadelphia"
   },
   {
     "order": 51,
+    "team_id": 23,
     "team": "Memphis"
   },
   {
     "order": 52,
+    "team_id": 8,
     "team": "Detroit"
   },
   {
     "order": 53,
+    "team_id": 24,
     "team": "New Orleans"
   },
   {
     "order": 54,
+    "team_id": 9,
     "team": "Indiana"
   },
   {
     "order": 55,
+    "team_id": 28,
     "team": "Oklahoma City"
   },
   {
     "order": 56,
+    "team_id": 12,
     "team": "Charlotte"
   },
   {
     "order": 57,
+    "team_id": 12,
     "team": "Charlotte"
   },
   {
     "order": 58,
+    "team_id": 3,
     "team": "New York"
   },
   {
     "order": 59,
+    "team_id": 2,
     "team": "Brooklyn"
   },
   {
     "order": 60,
+    "team_id": 9,
     "team": "Indiana"
   }
 ]
--- apps/drafter/src/app/draft-order.tsx
+++ apps/drafter/src/app/draft-order.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { List, ListItem } from '@material-ui/core';
+import { Avatar, List, ListItem, ListItemAvatar } from '@material-ui/core';
 import ListItemText from '@material-ui/core/ListItemText';
 
 interface DraftOrderProps {
@@ -22,6 +22,9 @@ export function DraftOrder(props: DraftOrderProps) {
   return <List>
     {draftOrder.map((draftOrderItem, index) => (
       <ListItem key={`item-${index}`}>
+        <ListItemAvatar>
+          <Avatar src={`https://assets-sports.thescore.com/basketball/team/${draftOrderItem["team_id"]}/small_logo.png`} />
+        </ListItemAvatar>
         <ListItemText key={`item-text-${index}`}
           secondary={`#${draftOrderItem["order"]}`} primary={`${draftOrderItem["team"]}`}
         />
