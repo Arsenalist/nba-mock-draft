ed83562626c1dfc23c29b657e1aab77b8043b622
Zarar Siddiqi <zarars@gmail.com>
2021-07-24 20:34:28 -0400

    Fix bug in URL

--- apps/drafter/src/app/main.tsx
+++ apps/drafter/src/app/main.tsx
@@ -25,7 +25,7 @@ export function Main() {
     const protocol = window.location.protocol;
     const domain = window.location.hostname;
     const port = window.location.port;
-    return `${protocol}://${domain}${port ? `:${port}` : ''}`;
+    return `${protocol}//${domain}${port ? `:${port}` : ''}`;
   }
 
   useEffect(() => {
